9fcae78c06b694c20cfe85f3bdd622d080211a64
William Okano <williamokano@gmail.com>
2015-03-26 22:39:54 -0400

    Added second activity and implemented a splash screen

---
+++ app/src/main/java/com/williamokano/apps/kidsworld/GameMain.java
@@ -0,0 +1,27 @@
+package com.williamokano.apps.kidsworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+
+public class GameMain extends Activity {
+
+    private float x1, x2;
+    static final int MIN_DISTANCE = 150;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_main);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+
+        }
+        return super.onTouchEvent(event);
+    }
+}
--- app/src/main/java/com/williamokano/apps/kidsworld/SplashScreen.java
+++ app/src/main/java/com/williamokano/apps/kidsworld/SplashScreen.java
@@ -4,6 +4,7 @@ import com.williamokano.apps.kidsworld.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,10 +13,34 @@ import android.view.View;
 
 public class SplashScreen extends Activity {
 
+    //Splash screen timer
+    private static int SPLASH_TIMEOUT = 3000; //3 seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash_screen);
+
+        /**
+         * Make a new handler to open with delay the main intent
+         * and then close this to prevent the user to come back
+         * because this activity must be shown just one time
+         */
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /**
+                 * Create the intent and start the activity
+                 */
+                Intent MainGameIntent = new Intent(SplashScreen.this, GameMain.class);
+                startActivity(MainGameIntent);
+
+                /**
+                 * Finish this activity to prevent from coming back
+                 */
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 }
