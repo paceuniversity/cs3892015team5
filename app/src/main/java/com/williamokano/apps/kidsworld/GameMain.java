717e01ec1cdde0c78fb2ddd629af26a21275cbcd
evapzy <yinhua@gdufe.edu.cn>
2015-04-07 22:43:46 +0800

    add the sounds

--- app/src/main/java/com/williamokano/apps/kidsworld/GameMain.java
+++ app/src/main/java/com/williamokano/apps/kidsworld/GameMain.java
@@ -1,7 +1,11 @@
 package com.williamokano.apps.kidsworld;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
@@ -13,9 +17,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.williamokano.apps.kidsworld.models.Image;
+import com.williamokano.apps.kidsworld.models.Sound;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 enum SwipeDirection {
     Left,
@@ -36,6 +42,10 @@ public class GameMain extends Activity {
 
     TextView imageDescription;
 
+    private SoundPool soundPool;
+    private HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+    private int currStreamId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -51,12 +61,17 @@ public class GameMain extends Activity {
         imageDog = new Image();
         imageCat = new Image();
 
+        Sound soundDog = new Sound(R.raw.dog);
+        Sound soundCat = new Sound(R.raw.cat);
+
         imageDog.setImageAsset(R.drawable.dog);
         imageDog.setDescription("DOG");
+        imageDog.setSoundAsset(soundDog);
         images.add(imageDog);
 
         imageCat.setImageAsset(R.drawable.cat);
         imageCat.setDescription("CAT");
+        imageCat.setSoundAsset(soundCat);
         images.add(imageCat);
 
 
@@ -74,45 +89,68 @@ public class GameMain extends Activity {
         colorWhite = new Image();
         colorYellow = new Image();
 
+        Sound soundBlack = new Sound(R.raw.black);
+        Sound soundBlue = new Sound(R.raw.blue);
+        Sound soundGreen = new Sound(R.raw.green);
+        Sound soundOrange = new Sound(R.raw.orange);
+        Sound soundPink = new Sound(R.raw.pink);
+        Sound soundPurple = new Sound(R.raw.purple);
+        Sound soundRed = new Sound(R.raw.red);
+        Sound soundWhite = new Sound(R.raw.white);
+        Sound soundYellow = new Sound(R.raw.yellow);
+
         colorBlack.setImageAsset(R.drawable.black);
         colorBlack.setDescription("BLACK");
+        colorBlack.setSoundAsset(soundBlack);
         images.add(colorBlack);
 
         colorBlue.setImageAsset(R.drawable.blue);
         colorBlue.setDescription("BLUE");
+        colorBlue.setSoundAsset(soundBlue);
         images.add(colorBlue);
 
         colorGreen.setImageAsset(R.drawable.green);
         colorGreen.setDescription("GREEN");
+        colorGreen.setSoundAsset(soundGreen);
         images.add(colorGreen);
 
         colorOrange.setImageAsset(R.drawable.orange);
         colorOrange.setDescription("ORANGE");
+        colorOrange.setSoundAsset(soundOrange);
         images.add(colorOrange);
 
         colorPink.setImageAsset(R.drawable.pink);
         colorPink.setDescription("PINK");
+        colorPink.setSoundAsset(soundPink);
         images.add(colorPink);
 
         colorPurple.setImageAsset(R.drawable.purple);
         colorPurple.setDescription("PURPLE");
+        colorPurple.setSoundAsset(soundPurple);
         images.add(colorPurple);
 
         colorRed.setImageAsset(R.drawable.red);
         colorRed.setDescription("RED");
+        colorRed.setSoundAsset(soundRed);
         images.add(colorRed);
 
         colorWhite.setImageAsset(R.drawable.white);
         colorWhite.setDescription("WHITE");
+        colorWhite.setSoundAsset(soundWhite);
         images.add(colorWhite);
 
         colorYellow.setImageAsset(R.drawable.yellow);
         colorYellow.setDescription("YELLOW");
+        colorYellow.setSoundAsset(soundYellow);
         images.add(colorYellow);
 
         // Initialize shapes
 
         Image shapeSquare, shapeRectangle, shapeCircle, shapeTriangle;
+        Sound soundCircle = new Sound(R.raw.circle);
+        Sound soundRectangle = new Sound(R.raw.rectangle);
+        Sound soundSquare = new Sound(R.raw.square);
+        Sound soundTriangle = new Sound(R.raw.triangle);
 
         shapeCircle = new Image();
         shapeRectangle = new Image();
@@ -121,18 +159,22 @@ public class GameMain extends Activity {
 
         shapeCircle.setImageAsset(R.drawable.circle);
         shapeCircle.setDescription("CIRCLE");
+        shapeCircle.setSoundAsset(soundCircle);
         images.add(shapeCircle);
 
         shapeRectangle.setImageAsset(R.drawable.rectangle);
         shapeRectangle.setDescription("RECTANGLE");
+        shapeRectangle.setSoundAsset(soundRectangle);
         images.add(shapeRectangle);
 
         shapeSquare.setImageAsset(R.drawable.square);
         shapeSquare.setDescription("SQUARE");
+        shapeSquare.setSoundAsset(soundSquare);
         images.add(shapeSquare);
 
         shapeTriangle.setImageAsset(R.drawable.triangle);
         shapeTriangle.setDescription("TRIANGLE");
+        shapeTriangle.setSoundAsset(soundTriangle);
         images.add(shapeTriangle);
 
 
@@ -146,15 +188,17 @@ public class GameMain extends Activity {
          * Find the instance of the imageview and set the first
          * image from the array, which is the dog!
          */
-        main_game_image = (ImageView)findViewById(R.id.main_game_image);
+        main_game_image = (ImageView) findViewById(R.id.main_game_image);
         updateImage();
 
+        initSoundPool();
     }
 
     /**
      * This method was overrided to implement a simple swipe
      * detection. The GestureDetection class wasn't needed
      * to avoid unnecessary waste of resources.
+     *
      * @param event
      * @return
      */
@@ -204,13 +248,12 @@ public class GameMain extends Activity {
                  * is bigger than the necessary distance to trigger, we call the
                  * updateImage function to update the imagem.
                  */
-                if(Math.abs(deltaX) > MIN_DISTANCE) {
+                if (Math.abs(deltaX) > MIN_DISTANCE) {
                     updateImage();
-                }else{
-
+                } else {
                     Toast toast = Toast.makeText(getApplicationContext(), images.get(actual).getDescription(), Toast.LENGTH_SHORT);
                     toast.show();
-
+                    play(actual+1, 0);
                 }
                 break;
         }
@@ -233,9 +276,9 @@ public class GameMain extends Activity {
          * have to calculate the modulus of a negative number, which was
          * triggering run-time errors on the application.
          */
-        if(swipeDirection == SwipeDirection.Left) {
+        if (swipeDirection == SwipeDirection.Left) {
             this.actual = (actual + size - 1) % size;
-        }else if(swipeDirection == SwipeDirection.Right) {
+        } else if (swipeDirection == SwipeDirection.Right) {
             this.actual = (actual + size + 1) % size;
         }
 
@@ -245,4 +288,27 @@ public class GameMain extends Activity {
         main_game_image.setBackgroundResource(images.get(this.actual).getImageAsset());
     }
 
+    @TargetApi(21)
+    private void initSoundPool() {
+        if ((android.os.Build.VERSION.SDK_INT) == 21) {
+            SoundPool.Builder soundPool21 = new SoundPool.Builder();
+            soundPool21.setMaxStreams(15);
+            soundPool = soundPool21.build();
+        } else {
+            soundPool = new SoundPool(15, AudioManager.STREAM_MUSIC, 0);
+        }
+        for (int i = 0; i< images.size(); i++ )
+        {
+            hashMap.put(i+1, soundPool.load(getApplicationContext(),images.get(i).getSoundAsset().getSoundAsset(), 1));
+        }
+    }
+
+    private void play(int sound, int loop) {
+        AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        float currVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        float maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        float volume = currVolume / maxVolume;
+        currStreamId = soundPool.play(hashMap.get(sound), volume, volume, 1, loop, 1.0f);
+        System.out.println(currStreamId);
+    }
 }
