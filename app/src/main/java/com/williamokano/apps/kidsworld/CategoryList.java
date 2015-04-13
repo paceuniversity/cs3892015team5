1a648c6c376e422cf764837adc76d838db49da9b
evapzy <yinhua@gdufe.edu.cn>
2015-04-13 12:10:58 +0800

    revise the list and add connection

--- app/src/main/java/com/williamokano/apps/kidsworld/CategoryList.java
+++ app/src/main/java/com/williamokano/apps/kidsworld/CategoryList.java
@@ -1,31 +1,65 @@
 package com.williamokano.apps.kidsworld;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Eva on 2015/4/11.
  */
 public class CategoryList extends Activity {
-    private String[] data = {"Shape","Color","Animal"};
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                CategoryList.this,android.R.layout.simple_list_item_1,data);
-        ListView listView = (ListView)findViewById(R.id.list_view);
+
+        ListView listView;
+        listView = (ListView)this.findViewById(R.id.listview);
+
+        SimpleAdapter adapter = new SimpleAdapter(this,getData(),R.layout.category_item,new String[]{"title","img"},new int[]{R.id.title,R.id.img});
         listView.setAdapter(adapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(CategoryList.this,GameMain.class);
-                startActivity(intent);
-            }
-        });
+                ListView lv = (ListView)parent;
+                HashMap<String,Object> map = (HashMap<String,Object>)lv.getItemAtPosition(position);
+                if(map.get("title")=="Shape"){
+                    Intent MainGameIntent = new Intent(CategoryList.this, GameMain.class);
+                    startActivity(MainGameIntent);
+                }
+            }});
+     }
+
+    private List<Map<String,Object>> getData(){
+       List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+
+       Map<String,Object> map = new HashMap<String,Object>();
+       map.put("title","Shape");
+       map.put("img",R.drawable.triangle);
+       list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("title","Color");
+        map.put("img",R.drawable.pink);
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("title","Animal");
+        map.put("img",R.drawable.cat);
+        list.add(map);
+
+        return list;
     }
 }
