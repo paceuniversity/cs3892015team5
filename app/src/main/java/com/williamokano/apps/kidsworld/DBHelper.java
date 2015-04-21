f02b904e90e0b6db738281fc69c091b9c6a616fe
jslucassf <jslucassf@gmail.com>
2015-04-21 00:04:47 -0500

    Partial changes in the DB structure.

--- app/src/main/java/com/williamokano/apps/kidsworld/DBHelper.java
+++ app/src/main/java/com/williamokano/apps/kidsworld/DBHelper.java
@@ -7,33 +7,90 @@ import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
 import android.database.Cursor;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.database.SQLException;
+import android.util.Log;
+
 /**
  * Created by JsLucasSf on 4/12/2015.
  */
-public class DBHelper extends SQLiteOpenHelper{
+public class DBHelper extends SQLiteOpenHelper {
 
-    static final String DB_NAME = "base.db";
+    static final String DB_NAME = "KidsWorldDB";
     static final int DB_VERSION = 1;
     static final String _ID = "_id";
-    static final String TABLE = "Objects";
+    static final String TABLE = "thing";
     static final String IMAGE = "image";
-    static final String OBJ_DESCRIPTION = "object_description";
+    static final String OBJ_DESCRIPTION = "description";
     static final String SOUND = "sound";
-    static final String CATEGORY = "category";
+    static final String CATEGORY = "category_id";
 
-    Context context;
+    private Context context;
 
-    public DBHelper(Context context){
+    private static String DB_PATH = "/data/data/com.williamokano.apps.kidsworld/databases/";
+    private SQLiteDatabase myDataBase;
+
+    public static String getDB_PATH() {
+        return DB_PATH;
+    }
+
+    public static void setDB_PATH(String DB_PATH) {
+        DBHelper.DB_PATH = DB_PATH;
+    }
+
+    public static String getDbName() {
+        return DB_NAME;
+    }
+
+    public static int getDbVersion() {
+        return DB_VERSION;
+    }
+
+    public static String getId() {
+        return _ID;
+    }
+
+    public static String getImage() {
+        return IMAGE;
+    }
+
+    public static String getTable() {
+        return TABLE;
+    }
+
+    public static String getObjDescription() {
+        return OBJ_DESCRIPTION;
+    }
+
+    public static String getSound() {
+        return SOUND;
+    }
+
+    public static String getCategory() {
+        return CATEGORY;
+    }
+
+    public DBHelper(Context context) {
 
         super(context, DB_NAME, null, DB_VERSION);
         this.context = context;
 
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db){
+    public SQLiteDatabase getMyDataBase() {
 
-        SQLiteDatabase dbTest = null;
+        return myDataBase;
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        /*SQLiteDatabase dbTest = null;
 
         try {
 
@@ -61,15 +118,104 @@ public class DBHelper extends SQLiteOpenHelper{
         }else{
             db = dbTest;
         }
+*/
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //To be implemented in the next sprints
+    }
+
+    public void createDataBase() throws IOException {
+
+        boolean dbExist = false; //checkDataBase();
+
+        if (dbExist) {
+            //do nothing - database already exist
+        } else {
+
+            //By calling this method and empty database will be created into the default system path
+            //of your application so we are gonna be able to overwrite that database with our database.
+            this.getReadableDatabase();
+
+            try {
+
+                copyDataBase();
+
+            } catch (IOException e) {
+
+                throw new Error("Error copying database");
+
+            }
+        }
+
+    }
+
+    private boolean checkDataBase() {
+
+        SQLiteDatabase checkDB = null;
+
+        try {
+            String myPath = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+        } catch (SQLException e) {
+            //database does't exist yet.
+        }
+
+        if (checkDB != null) {
+
+            checkDB.close();
+
+        }
+
+        return checkDB != null ? true : false;
+    }
+
+    private void copyDataBase() throws IOException {
+
+        //Open your local db as the input stream
+        InputStream myInput = context.getAssets().open(DB_NAME);
+
+        // Path to the just created empty db
+        String outFileName = DB_PATH + DB_NAME;
+
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+
+    }
+
+    public void openDataBase() throws SQLException {
+
+        //Open the database
+        String myPath = DB_PATH + DB_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        //To be implemented in the next sprints
+    public synchronized void close() {
+
+        if (myDataBase != null)
+            myDataBase.close();
+
+        super.close();
+
     }
 
-    public void initializeDefaultComponents(SQLiteDatabase db){
+/*    public void initializeDefaultComponents(SQLiteDatabase db){
 
         ContentValues values = new ContentValues();
 
@@ -193,5 +339,5 @@ public class DBHelper extends SQLiteOpenHelper{
 
         db.insertOrThrow(this.TABLE, null, values);
         values.clear();
-    }
+    }*/
 }
--- app/src/main/java/com/williamokano/apps/kidsworld/bll/ThingBLL.java
+++ app/src/main/java/com/williamokano/apps/kidsworld/bll/ThingBLL.java
@@ -1,8 +1,14 @@
 package com.williamokano.apps.kidsworld.bll;
 
+import android.content.Context;
+import android.database.Cursor;
+
+import com.williamokano.apps.kidsworld.DBHelper;
 import com.williamokano.apps.kidsworld.models.Category;
+import com.williamokano.apps.kidsworld.models.Image;
 import com.williamokano.apps.kidsworld.models.Thing;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -11,7 +17,30 @@ import java.util.ArrayList;
 public class ThingBLL {
 
     //@TODO: Implement this method. Retrieve from database
-    public ArrayList<Thing> GetThings() {
+    public ArrayList<Thing> GetThings(DBHelper dbHelper) {
+
+        ArrayList<Thing> things = new ArrayList<>();
+
+        try {
+            dbHelper.createDataBase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dbHelper.openDataBase();
+
+        String[] columns = {dbHelper.getImage(), dbHelper.getSound(), dbHelper.getObjDescription(),dbHelper.getCategory()};
+
+        Cursor c = dbHelper.getMyDataBase().query(dbHelper.getTable(), columns, null, null, null, null, null);
+
+        c.moveToFirst();
+
+        while(!c.isAfterLast()){
+
+            things.add(new Thing());
+
+
+        }
         return null;
     }
 
